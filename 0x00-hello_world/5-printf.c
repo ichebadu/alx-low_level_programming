63a46fe2c91903c89e2217834ab0f4be3bd91d82
ichebadu <chukwu.iche@gmail.com>
2022-09-08 23:34:04 -0700

    using printf to print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,12 @@
-#include <stdio.h>                                                                                     
-
-                                                                                              
-/**                                                                                                    
-
-* main - print the string in the put function                                                              
-
-*                                                                                                   
-* this program prints "Programing is like building a multilingual puzzle                                  
-
-* Return: 0                                                                                            
-
-*/                                                                                                     
-
-int main(void)                                                                                         
-{                                                                                             
-
+#include <stdio.h>                                                                                              
+/**                                                                                        
+* main - print the string in the put function                                             
+*           
+* this program prints "Programing is like building a multilingual puzzle                   
+* Return: 0                                                                                
+*/                                                                                         
+int main(void)            
+{                                                                                          
         printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

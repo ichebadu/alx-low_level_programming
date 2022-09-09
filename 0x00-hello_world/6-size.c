868fa5661f1d0667c3f81129d5fdf8fbd9b9779a
ichebadu <chukwu.iche@gmail.com>
2022-09-09 01:58:01 -0700

    printing data type with printf in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	char c;
 	int i;
 	long li;
-        long long lli;
-        float f;
+	long long lli;
+	float f;
 	
-        printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));

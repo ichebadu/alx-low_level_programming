8f101a8ce0a6e96743db0ffede5b8c06407763d2
ichebadu <chukwu.iche@gmail.com>
2022-09-08 22:02:18 -0700

    low level c language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,13 +4,13 @@
 
 /**                                                                                                    
 
-* main - print the string in the function                                                              
+* main - print the string in the put function                                                              
 
 *                                                                                                      
 
 * Description: using the main function                                                                 
 
-* this program prints "Program is like building a multilingual puzzle                                  
+* this program prints "Programing is like building a multilingual puzzle                                  
 
 * Return: 0                                                                                            
 

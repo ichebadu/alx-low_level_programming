23cfca9eb5477c35f55e744f2aa0aeef49ad4821
ichebadu <chukwu.iche@gmail.com>
2022-09-08 21:57:31 -0700

    low level c languae

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>                                                                                     
+
+                                                                                                       
+
+/**                                                                                                    
+
+* main - print the string in the function                                                              
+
+*                                                                                                      
+
+* Description: using the main function                                                                 
+
+* this program prints "Program is like building a multilingual puzzle                                  
+
+* Return: 0                                                                                            
+
+*/                                                                                                     
+
+int main(void)                                                                                         
+
+{                                                                                                      
+
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,73 @@
+                                                                                              
+
+                                                                                              
+
+                                                                                              
+
+/**                                                                                           
+
+                                                                                              
+
+* main - print the string in the put function                                                 
+
+                                                                                              
+
+*                                                                                             
+
+                                                                                              
+
+* Description: using the main function                                                        
+
+                                                                                              
+
+* this program prints *Programing is like building a multiligual puzzle                       
+
+                                                                                              
+
+* Return: 0                                                                                   
+
+                                                                                              
+
+*/                                                                                            
+
+                                                                                              
+
+int main(void)                                                                                
+
+                                                                                              
+
+{                                                                                             
+
+                                                                                              
+
+        char c;                                                                               
+
+                                                                                              
+
+        int i;                                                                                
+
+                                                                                              
+
+        long li;                                                                              
+
+                                                                                              
+
+        long long lli;                                                                        
+
+                                                                                              
+
+        float f;                                                                              
+
+                                                                                              
+
+                                                                                              
+
+                                                                                              
+
+        printf("Size of a char: %ld byte(s)\n", sizeof(c)); 
+	printf("Size of a char: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long : %ld byte(s)\n", sizeof(li));
+	printf("Size of a long  long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a long : %ld byte(s)\n", sizeof(f));
+	Return (0);
+}

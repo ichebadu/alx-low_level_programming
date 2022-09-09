528847b8c51b19d83440652fec8a7bf6a13fe0a6
ichebadu <chukwu.iche@gmail.com>
2022-09-09 03:53:35 -0700

    printing char int float in C programming languagex

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,8 @@
  *
  * Description: using the main function
  * this program prints *Programing is like building a multilinual puzzle
-*/ Return:
+ * Return: 0
+ */
 int main(void)
 {
 	char c;
@@ -17,6 +18,6 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli))
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	return (0);
 }

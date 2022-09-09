b119bb3f9fff242b2b15ff5c324c1e0eebd45fce
ichebadu <chukwu.iche@gmail.com>
2022-09-09 01:38:01 -0700

    printing data type with printf in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - print the string in the put function
- *
- * Description: using the main function
- * this program prints *Programing is like building a multilingual puzzle
- * Return: 0
+* main - print the string in the put function
+*   
+* Description: using the main function
+* this program prints *Programing is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
-{                            
+{
         char c;
         int i;
         long li;

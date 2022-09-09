6508dfcf8813dcb0df8a29e040657e16ba36bf48
ichebadu <chukwu.iche@gmail.com>
2022-09-09 00:38:48 -0700

    printing char int float and long using printf in c programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,16 @@
 * Return: 0
 */
 int main(void)
-{                                                                                             
+{                            
         char c;
         int i;
         long li;
         long long lli;
         float f;                                                  
-
         printf("Size of a char: %ld byte(s)\n", sizeof(c)); 
 	printf("Size of a int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	Return (0);
+	return (0);
 }

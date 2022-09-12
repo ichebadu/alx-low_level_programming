94b43eb412fe9bc91f7361683a401c84f2ca02c4
ichebadu <chukwu.iche@gmail.com>
2022-09-12 12:25:28 -0700

    inittial commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 
 /**
  * main - main block
- * Description: prints the alphabet in lowercase,
+ * Description: prints the lowercase alphabet in reverse,
  * followed by a new line
  * Return: Always 0 (Success)
  */
@@ -10,7 +10,7 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
 	}

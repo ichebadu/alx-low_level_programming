d36c1dc7b9dee1d23e225d2f1a455c42b6ff6b28
ichebadu <chukwu.iche@gmail.com>
2022-09-13 13:31:02 -0700

    function_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,10 +10,10 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_baver(void);
-void times _table(void);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void  num(int r, int n);
+void num(int r, int n);
 
 #endif

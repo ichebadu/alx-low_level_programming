4ac202ba4deeef49ba7aa9eaa73762e30a3c5dfe
ichebadu <chukwu.iche@gmail.com>
2022-09-14 12:18:50 -0700

    function nested_loop

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the sign of a number
+ * @n: type int integer, negative or positive
+ * Description: print +, 0, or - depending on number
+ * Return: 1 if +, 0 if 0, and -1 if -
+ */
+
+int print_sign(int n)
+
+{
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+
+	}
+
+	else
+
+		if (n < 0)
+
+		{
+
+		_putchar('-');
+		return (-1)
+		}
+
+		else
+
+		{
+
+		_putchar('+');
+
+		return (1);
+		}
+
+}

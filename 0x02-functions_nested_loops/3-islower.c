047e414d952a199cac34237fe2a419b553adf273
ichebadu <chukwu.iche@gmail.com>
2022-09-14 11:45:59 -0700

    function_nested_loop

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _islower - Return 1 if alphabet is lower, 0 if it isn't.
+ * @c: char type letter
+ *  Return: 1 if letter is lower case, and 0 if otherwise
+ */
+
+int _islower(int c)
+
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}

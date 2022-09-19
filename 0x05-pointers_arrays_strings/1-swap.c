e911fee691e306c036850ab5b590b08a1920b5e3
ichebadu <chukwu.iche@gmail.com>
2022-09-19 10:55:15 -0700

    swaping to integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int -> given two integers swap the values they are holding
- * @: parameter 1
- * @: parameter 2
+ * @a: parameter 1
+ * @b: parameter 2
  *
  */
 void swap_int(int *a, int *b)

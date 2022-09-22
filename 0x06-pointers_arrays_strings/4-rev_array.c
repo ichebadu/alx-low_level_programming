6a4098d872bb7760a2b2cc9c6a09b7fac56740d6
ichebadu <chukwu.iche@gmail.com>
2022-09-22 10:30:55 -0700

    writing a function to reverses an array of integers just like standard library provides a similar function: strncpy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
 /**
  * reverse_array - reverses an array of integers
  * @a: array to be reversed
- * n: number of elements in the array
+ * @n: number of elements in the array
  */
 void reverse_array(int *a, int n)
 {

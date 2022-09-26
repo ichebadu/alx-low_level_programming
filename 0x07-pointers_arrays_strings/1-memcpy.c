5de426d696c5957b940bc0cad225960a7fcc47b5
ichebadu <chukwu.iche@gmail.com>
2022-09-26 12:25:25 -0700

    function set memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,11 @@
 
 /**
  * *_memcpy - copies memory area
- * @dest: destination memory
- * @src: memory area to copy data from
- * @n: number of bytes copy
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
  *
- *  * Return: pointer to dest
+ * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {

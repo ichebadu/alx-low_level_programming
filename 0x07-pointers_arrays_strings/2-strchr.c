65fc1b29adde6b1f62b1a69bf54e3b9cd505df7f
ichebadu <chukwu.iche@gmail.com>
2022-09-27 07:26:55 -0700

    Write a function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,15 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for(i = 0; *(s + i); i++)
+	for (i = 0; *(s + i); i++)
 	{
-		if(*(s + 1) == c)
+		if (*(s + 1) == c)
 			return (s + i);
 	}
+
 	if (*(s + i) == c)
+	{
 		return (s + i);
+	}
 	return (0);
 }

ed509a03deafb8cc0da9d613b339b36d9ac02f68
ichebadu <chukwu.iche@gmail.com>
2022-10-04 06:29:29 -0700

    Write a program that prints the number of arguments passed into it followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function prints the number of argument follwed by a line
+ * @argc: argc parameter
+ * @argv: array of a command listed
+ * Return: 0
+ */
+
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("the number of argument is %d\n", argc);

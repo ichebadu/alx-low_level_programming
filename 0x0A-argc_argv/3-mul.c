f4fc14772663b13c0dcfd1a5eb8cc6d787425ff1
ichebadu <chukwu.iche@gmail.com>
2022-10-04 08:44:25 -0700

    Write a program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - function prints two line multiplication
+ * @argc: argc parameter
+ * @argv: an array enter from command line
+ * @n1: first number
+ * @n2: second number
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int multiplcationTable, n1, n2;
+
+	for (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	multiplicationTable = n1 * n2;
+
+	printf("%d\n", multiplicationTable);
+	return (0);
+}

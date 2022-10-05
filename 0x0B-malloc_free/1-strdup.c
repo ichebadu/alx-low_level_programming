4633245a31ca99c8b5b5c89fb9d2d273e26a3057
ichebadu <chukwu.iche@gmail.com>
2022-10-05 05:29:14 -0700

    Write a function that returns a pointer to a newly aace in memory, which contains a copy of the string given as a paramet

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,16 @@
-#include "main"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * *_strdup - returns a pointer to a memory space that was just allocated,
+ * _strdup - returns a pointer to a memory space that was just allocated,
  *  which duplicates the string specified as an argument.
  * @str: the string parameter
  * Return: returns NULL if str = NULL
  * the _strdup function returns a pointer to the duplicated string.
  * In the event that there was not enough memory, it returns NULL.
  */
+
 char *_strdup(char *str)
 {
 	char *copy;

d3cb4d6a6e65bbcd36e8d29476aff7ca6835a338
ichebadu <chukwu.iche@gmail.com>
2022-10-05 17:55:46 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - memory function
+ * @s: string memory area to be filled
+ * @b: a character to copy
+ * @n: an n number of times to copy b
+ *
+ * Return: a string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to check
+ * @c: char to check
+ *
+ * Return: a pointer to the first occurrence s with c character or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+
+	if (*(s + i) == c)
+	{
+		return (s + i);
+	}
+	return (0);
+}

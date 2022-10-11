48ff5c82d7d72fe41b38ff4a48bfc929ce8ade05
ichebadu <chukwu.iche@gmail.com>
2022-10-11 13:46:16 -0700

    all structure task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,61 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * *new_dog - Write a function that creates a new dog.
- * @name: element name of the dog
- * @age: element age of the dog
- * @owner: element owener of the dog
- * Return: pointer to nwdog
+ * *_strcpy - prototype function
+ * @dest: Variable char pointer
+ * @src: Variable char pointer
+ * Return: dest pointer
  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0' ; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	dest[a] = '\0';
+
+	return (dest);
+}
+
+/**
+* *new_dog - Prototype
+* @name: Variable char
+* @age: Variable float
+* @owner: Variable char
+* Return: new dog
+*/
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *nwdog;
-	char *nwname, *nwowner;
-	int a = 0, b = 0, c = 0, d = 0;
+	dog_t *hotdog = NULL;
+	int a, b;
 
-	nwdog = malloc(sizeof(struct dog));
-	if (nwdog == NULL)
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	hotdog = malloc(sizeof(dog_t));
+	if (hotdog == NULL)
 		return (NULL);
 	for (a = 0; name[a] != '\0'; a++)
 		;
 	for (b = 0; owner[b] != '\0'; b++)
 		;
-	nwname = malloc(sizeof(char) * (a + 1));
-		if (nwname == NULL)
+	hotdog->name = malloc(sizeof(char) * (a + 1));
+	hotdog->owner = malloc(sizeof(char) * (b + 1));
+	if (hotdog->name == NULL || hotdog->owner == NULL)
+	{
+		free(hotdog->name);
+		free(hotdog->owner);
+		free(hotdog);
 		return (NULL);
-	nwowner = malloc(sizeof(char) * (b + 1));
-		if (nwowner == NULL)
-		return (NULL);
-	for (c = 0; c <= a; c++)
-	nwname[c] = name[c];
-	for (d = 0; d <= b; d++)
-	nwowner[d] = owner[d];
-	nwdog->name = nwname;
-	nwdog->age = age;
-	nwdog->owner = owner;
-	return (nwdog);
+	}
+	_strcpy(hotdog->name, name);
+	_strcpy(hotdog->owner, owner);
+	hotdog->age = age;
+	return (hotdog);
 }

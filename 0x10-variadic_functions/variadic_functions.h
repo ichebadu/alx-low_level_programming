5a837cbb8f6188e0888e602a49f7ae7c14ade86e
ichebadu <chukwu.iche@gmail.com>
2022-10-14 12:38:40 -0700

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef Variadic_Functions_H
+#define Variadic_Functions_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif

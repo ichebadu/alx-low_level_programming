0fb7e1650a9df0fce3d4a38e23da00e38a1868eb
ichebadu <chukwu.iche@gmail.com>
2022-12-08 06:08:00 -0800

    modified

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,44 +1,53 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert a node in a idx
- * @h: the list to insert the node
- * @idx: the index
- * @n: number to add in the new node
- * Return: the node
- */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+ * insert_dnodeint_at_index - inserts a new node at given index in the list
+ * @head: pointer to head of the list
+ * @idx: index to add at, starting from 0
+ * @n: value of new node
+ * Return: new node or null
+ **/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 {
-	dlistint_t *actual;
-	dlistint_t *new;
-	size_t size;
+	unsigned int count;
+	dlistint_t *tmp, *new, *tmp_prev;
 
-
-	actual = *h;
-	size = 0;
-	if (h == NULL)
-		return (NULL);
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	while (actual != NULL)
-	{
-		size++;
-		actual = actual->next;
-	}
-	if (size < idx)
-		return (NULL);
-	if (size == idx)
-		return (add_dnodeint_end(h, n));
-	actual = *h;
-	while (--idx)
-		actual = actual->next;
+	if (head == NULL && idx > 0)
+	return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->next = actual->next;
-	actual->next->prev = new;
-	new->prev = actual;
-	actual->next = new;
-	return (new);
+	new->n = n, new->prev = new->next = NULL;
+
+	if (idx == 0)
+	{
+		if (*head)
+	{
+		new->next = *head;
+		(*head)->prev = new, *head = new;
+	}
+		else
+			*head = new;
+		return (new);
+	}
+	count = 1, tmp = (*head)->next;
+	while (tmp)
+	{
+		if (idx == count)
+		{
+			tmp->prev->next = new, new->prev = tmp->prev;
+			new->next = tmp, tmp->prev = new;
+			return (new);
+		}
+		count++;
+		tmp_prev = tmp;
+		tmp = tmp->next;
+	}
+	if (tmp == NULL && count == idx)
+	{
+		tmp_prev->next = new, new->prev = tmp_prev;
+		return (new);
+	}
+	free(new);
+	return (NULL);
 }
